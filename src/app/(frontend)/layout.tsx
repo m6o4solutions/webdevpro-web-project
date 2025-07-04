e1a61d9e5421012c8f4d717fb03ba2f6e7a5c1a1
Michael Owuor <michael.owuor@gmail.com>
2025-07-04 19:42:41 +0300

    chore: miscellaneous updates and fixes (#1)
    * chore: Update RESEND_FROM_EMAIL to correct domain
    
    * feat: Add Clarity tracking script to layout
    
    * chore: update project dependencies

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Script from "next/script";
 import { Comfortaa as FontHeader, Montserrat as FontBody } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
@@ -27,6 +28,15 @@ const RootLayout = async ({ children }: Readonly<{ children: ReactNode }>) => {
 			<html lang="en" suppressHydrationWarning className={cn(fontHeader.variable, fontBody.variable)}>
 				<head>
 					<link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+					<Script id="clarity-script" strategy="afterInteractive">
+						{`
+							(function(c,l,a,r,i,t,y){
+								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+							})(window, document, "clarity", "script", "s8jgs2y8b0");
+						`}
+					</Script>
 				</head>
 
 				<body className="font-body flex h-screen flex-col">
